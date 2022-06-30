dc8138a8b368b56120586ede555e369f41349811
Gurav-pranjali <67546558+Gurav-pranjali@users.noreply.github.com>
2022-06-30 05:48:05 +0530

    final change

---
+++ Back-End Spring-Boot/Authorization/Authorization/bin/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+drop table if exists userrecord;
+create table userrecord(user_id int primary key ,user_name varchar(30),user_password varchar(20));
+
+
+INSERT INTO userrecord (user_id,user_name,user_password) VALUES (101,'Pranjali','1234');
--- Back-End Spring-Boot/MutualFund/MutualFund/src/main/java/com/cts/mutualfund/MutualFundApplication.java
+++ Back-End Spring-Boot/MutualFund/MutualFund/src/main/java/com/cts/mutualfund/MutualFundApplication.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
